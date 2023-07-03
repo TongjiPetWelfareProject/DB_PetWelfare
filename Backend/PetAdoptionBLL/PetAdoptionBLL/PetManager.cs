a71ac18642f3270261161f2802e5fa81a0153f06
Victor Hu <103842499+VictorHuu@users.noreply.github.com>
2023-07-03 15:59:53 +0800

    Add files via upload

---
+++ Backend/PetAdoptionBLL/PetAdoptionBLL/PetManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PetAdopt.DAL;
+namespace PetAdopt.BLL
+{
+    public class PetManager
+    {
+        public int GetPet(Pet pet)
+        {
+            return new PetServer().GetPet(pet);
+        }
+    }
+}
