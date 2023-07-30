acfc81d5c8a9655b016723b0752fc5fcbe10caa8
shiguangbuyi <1664524275@qq.com>
2023-07-30 16:29:16 +0800

    将登录界面嵌入主页，暂未实现登录id传入和cookie

--- 用户界面/src/main.js
+++ 用户界面/src/main.js
@@ -2,10 +2,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import App from './App.vue'
+import MainApp from './mainpage.vue'
 import router from './router'
-const app = createApp(App)
 
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+const mainapp = createApp(MainApp)
+
+mainapp.use(ElementPlus)
+mainapp.use(router)
+mainapp.mount('#mainapp')
--- 用户界面/src/router/index.js
+++ 用户界面/src/router/index.js
@@ -1,22 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import pet_foster from '@/components/pet_foster.vue';
-import first_page from '@/components/first_page.vue';
-import donate from '@/components/donate.vue';
-import notice from '@/components/notice.vue'
-import medical from '@/components/medical.vue'
-import reservationdoctor from '@/components/reservationdoctor.vue'
-import pet_adopt from '@/components/pet_adopt.vue'
-import pet_details from '@/components/pet_details.vue'
-import mypage from '@/components/mypage.vue'
-import pet_adopt_form from '@/components/pet_adopt_form.vue'
-import forum from '@/components/forum.vue'
-import posting from '@/components/posting.vue'
-import pet_foster_table from '@/components/pet_foster_table.vue'
-import post_details from '@/components/post_details.vue'
+import pet_foster from '@/components/用户界面/pet_foster.vue';
+import first_page from '@/components/用户界面/first_page.vue';
+import donate from '@/components/用户界面/donate.vue';
+import notice from '@/components/用户界面/notice.vue'
+import medical from '@/components/用户界面/medical.vue'
+import reservationdoctor from '@/components/用户界面/reservationdoctor.vue'
+import pet_adopt from '@/components/用户界面/pet_adopt.vue'
+import pet_details from '@/components/用户界面/pet_details.vue'
+import mypage from '@/components/用户界面/mypage.vue'
+import pet_adopt_form from '@/components/用户界面/pet_adopt_form.vue'
+import forum from '@/components/用户界面/forum.vue'
+import posting from '@/components/用户界面/posting.vue'
+import pet_foster_table from '@/components/用户界面/pet_foster_table.vue'
+import post_details from '@/components/用户界面/post_details.vue'
+
+import login from '@/components/登录注册/Login.vue'
+import register from '@/components/登录注册/Register.vue'
 
 const routes = [
+    { path: '/', component: first_page },
     { path: '/pet_foster', component: pet_foster },
-    { path: '/first_page', component: first_page },
     { path: '/donate', component: donate },
     { path: '/notice', component: notice},
     { path: '/medical', component: medical},
@@ -29,6 +32,8 @@ const routes = [
     { path: '/posting', component: posting},
     { path: '/pet_foster_table', component: pet_foster_table},
     { path: '/post_details', component: post_details},
+    { path: '/login', component: login},
+    { path: '/register', component: register},
 ];
 
 const router = createRouter({
