ee6128cc27796fb58119e496dfd2152a0a9e0389
charlotteyanggg <104919289+llRagdoll@users.noreply.github.com>
2023-08-09 16:57:22 +0800

    更新医疗和公告api

--- 管理员界面/src/api/gg_rqb_jk.js
+++ 管理员界面/src/api/gg_rqb_jk.js
@@ -19,4 +19,53 @@ export default {
         throw new Error('获取捐款记录数据时出错：' + error.message);
       });
   },
+  //发送新公告
+  sendNewNoticeAPI(employeeId, title, content, time) {
+    const data = {
+      employeeId: employeeId,
+      title: title,
+      content: content,
+      time: time
+    };
+  
+    return axios
+      .post('/api/send-notice', data)
+      .then(response => response.data)
+      .catch(error => {
+        throw new Error('发送公告数据时出错：' + error.message);
+      });
+  },
+  getNoticeContentAPI(noticeId) {
+    return axios
+      .get(`/api/get-notice-content/${noticeId}`)
+      .then(response => response.data)
+      .catch(error => {
+        throw new Error('获取公告内容数据时出错：' + error.message);
+      });
+  },
+  //发送编辑过的公告
+  sendEditedNoticeAPI(employeeId, title, content, time) {
+    const data = {
+      employeeId: employeeId,
+      title: title,
+      content: content,
+      time: time
+    };
+  
+    return axios
+      .post('/api/send-notice', data)
+      .then(response => response.data)
+      .catch(error => {
+        throw new Error('发送公告数据时出错：' + error.message);
+      });
+  },
+  //删除公告的api
+  deleteNoticeAPI(noticeId) {
+    return axios
+      .delete(`/api/delete-notice/${noticeId}`) // 使用 DELETE 请求删除公告
+      .then(response => response.data)
+      .catch(error => {
+        throw new Error('删除公告时出错：' + error.message);
+      });
+  }
 };
