d2ea5b90ddd4ecaffb4de2b3ab7b376d6e870150
charlotteyanggg <104919289+llRagdoll@users.noreply.github.com>
2023-08-17 12:33:40 +0800

    更新预约界面

--- 总前端界面/src/components/用户界面/reservationdoctor.vue
+++ 总前端界面/src/components/用户界面/reservationdoctor.vue
@@ -1,12 +1,12 @@
 <template>
-     <el-text size="large" tag="b">预约问诊</el-text>
+     <el-text size="large" tag="b" style="margin-left:50px">预约问诊</el-text>
      <br><br>
     <el-form :model="form" label-width="120px" style="max-width: 90%;">
       <el-form-item label="宠物姓名">
         <el-input v-model="form.name"/>
       </el-form-item>
       <el-form-item label="种类">
-        <el-select v-model="form.kind" placeholder="please select a type">
+        <el-select v-model="form.kind" placeholder="请选择问题种类">
           <el-option label="皮肤问题" value="皮肤问题" />
           <el-option label="胃部问题" value="胃部问题" />
           <el-option label="耳部感染" value="耳部感染" />
@@ -20,12 +20,22 @@
           <el-option label="其他" value="其他" />
         </el-select>
       </el-form-item>
+      <el-form-item label="预约医生">
+        <el-select v-model="form.selectedDoctorID" placeholder="请选择医生">
+          <el-option
+            v-for="doctor in doctors"
+            :key="doctor.id"
+            :label="doctor.name"
+            :value="doctor.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="预约时间">
         <el-col :span="11">
           <el-date-picker
             v-model="form.date1"
             type="date"
-            placeholder="Pick a date"
+            placeholder="请选择时间"
             style="width: 100%"
           />
         </el-col>
@@ -50,38 +60,49 @@
         <el-input v-model="form.desc" type="textarea" />
       </el-form-item>
       <el-form-item>
+        <br><br>
         <el-button type="primary" @click="onSubmit">提交</el-button>
-        <el-button>取消</el-button>
+        <!-- <el-button>取消</el-button> -->
       </el-form-item>
     </el-form>
   </template>
   
 <script>
-import { defineComponent, reactive } from 'vue';
-import submitAppointmentAPI from '@/api/medical_donate';
+import { ref,defineComponent, reactive,onMounted } from 'vue';
+import medical_donate from '@/api/medical_donate';
 
 export default defineComponent({
   setup() {
+
+    const doctors = ref([]);
+   
+
     const form = reactive({
       name: '',
       kind: '',
       date1: '',
       desc: '',
+      selectedDoctorID:'',
     });
 
     const onSubmit = async () => {
       try {
         // 调用 submitAppointmentAPI 函数并传入表单数据
-        const response = await submitAppointmentAPI(form);
+        const response = await medical_donate.submitAppointmentAPI(form);
         console.log('提交成功：', response);
       } catch (error) {
         console.error('提交数据时出错：', error);
       }
     };
 
+    onMounted(async () => {
+      doctors.value = await medical_donate.getDoctorsAPI();
+    });
+
     return {
       form,
       onSubmit,
+      doctors,
     };
   },
 });
