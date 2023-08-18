c39f2a01b8293cbdabf0a5f520e9a29b562801b7
thesamechen <105065838+thesamechen@users.noreply.github.com>
2023-08-18 12:15:41 +0800

    Add files via upload

---
+++ src/api/notice_forum.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export default {
+    //获取公告ID，标题和内容
+    bulletinAPI() {
+        return axios
+            .get('/api/bulletin')
+            .then((response) => response.data)
+            .catch(error => {
+                throw new Error('获取公告数据时出错：' + error.message);
+            });
+    },
+};
+
+export const getPosts = async () => {
+    try {  
+        const response = await axios.get('/api/posts');
+        return response.data;
+     } catch (error) {
+            console.error('获取帖子列表时出错:', error);
+            throw error;  
+        }
+};
+export const searchPosts = async(searchText)=> {
+    try {
+    const response = await axios.get('/api/posts-search', { params: { searchText } });
+     return response.data; } 
+     catch (error) {
+     console.error('搜索帖子时出错:', error);
+     throw error;
+    }
+};
+                
+                
+
