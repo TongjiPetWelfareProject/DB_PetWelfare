92ace2e53ca1d0233e2dabc72f343be29e203b5e
DaenerysTar <132213561+DaenerysTar@users.noreply.github.com>
2023-08-19 00:01:06 +0800

    Add files via upload
    意识到还没有做点赞评论收藏阅读接口，补上了

--- src/api/pet_adopt.js
+++ src/api/pet_adopt.js
@@ -44,3 +44,65 @@ export const getPetDetails = (petId) => {
       throw error;
     });
 };
+
+export const submitLike = (userInfo, petInfo) => {
+  const likeData = {
+    user: userInfo.User_ID,
+    pet: petInfo.Pet_ID,
+  };
+
+  return axios.post('/api/pet-like', likeData)  // 假设后端有名为 pet-like 的接口来处理点赞
+    .then(response => {
+      return response.data; // 你可能希望在这里返回响应数据
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const submitFavorite = (userInfo, petInfo) => {
+  const favoriteData = {
+    user: userInfo.User_ID,
+    pet: petInfo.Pet_ID,
+  };
+
+  return axios.post('/api/pet-favorite', favoriteData)  // 假设后端有名为 pet-favorite 的接口来处理收藏请求
+    .then(response => {
+      return response.data; // 你可能希望在这里返回响应数据
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const submitReading = (userInfo, petInfo) => {
+  const readingData = {
+    user: userInfo.User_ID,
+    pet: petInfo.Pet_ID,
+  };
+
+  return axios.post('/api/pet-reading', readingData)  // 假设后端有名为 article-reading 的接口来处理阅读请求
+    .then(response => {
+      return response.data; // 你可能希望在这里返回响应数据
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const submitComment = (userInfo, petInfo) => {
+  const commentData = {
+    user: userInfo.User_ID,
+    pet: petInfo.Pet_ID,
+    text: commentText,
+    time: commentTime
+  };
+
+  return axios.post('/api/pet-comment', commentData)  // 假设后端有名为 article-comment 的接口来处理评论请求
+    .then(response => {
+      return response.data; // 你可能希望在这里返回响应数据
+    })
+    .catch(error => {
+      throw error;
+    });
+};c
