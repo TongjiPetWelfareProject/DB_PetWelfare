65f8387ded822500c7e7ad09713f265712117f88
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-20 19:51:54 +0800

    Update pet_foster.js

--- src/api/pet_foster.js
+++ src/api/pet_foster.js
@@ -1,7 +1,8 @@
 // 寄养申请表接口
 import axios from 'axios';
 
-const submitFosterApplication = ({user, name, type, size, date, num, remark}) => {
+export default { 
+  submitFosterApplication({user, name, type, size, date, num, remark}){
     const fosterData = {
       user,
       name,
@@ -20,6 +21,5 @@ const submitFosterApplication = ({user, name, type, size, date, num, remark}) =>
       console.log("发送失败")
       throw error;
     });
-}
-
-export default submitFosterApplication;
+  }
+};
