f49de6776ec108a7f792c7892581974af15383cb
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-20 15:59:07 +0800

    更新接口写法，可以渲染数据库帖子

--- src/api/notice_forum.js
+++ src/api/notice_forum.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default {
     //获取公告ID，标题和内容
     bulletinAPI() {
-        console.log("发出公告请求");
         return axios
             .get('/api/bulletin')
             .then((response) => response.data)
@@ -11,17 +10,17 @@ export default {
                 throw new Error('获取公告数据时出错：' + error.message);
             });
     },
+
+    getposts() {
+        return axios
+            .get('/api/forum')
+            .then((response) => response.data)
+            .catch(error => {
+                throw new Error('获取帖子数据时出错：' + error.message);
+            });
+    }
 };
 
-export const getPosts = async () => {
-    try {  
-        const response = await axios.get('/api/posts');
-        return response.data;
-     } catch (error) {
-            console.error('获取帖子列表时出错:', error);
-            throw error;  
-        }
-};
 export const searchPosts = async(searchText)=> {
     try {
     const response = await axios.get('/api/posts-search', { params: { searchText } });
@@ -31,5 +30,3 @@ export const searchPosts = async(searchText)=> {
      throw error;
     }
 };
-                
-                
