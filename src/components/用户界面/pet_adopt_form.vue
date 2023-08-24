3a465d0b916f6ed826eb638407d11ff8acf35c3e
DaenerysTar <132213561+DaenerysTar@users.noreply.github.com>
2023-08-24 22:18:47 +0800

    领养申请表加了动态路由

--- src/components/用户界面/pet_adopt_form.vue
+++ src/components/用户界面/pet_adopt_form.vue
@@ -1,77 +1,67 @@
 <template>
   <div class="tableitem" >
-    <img src="./photos/柯基头像.jpg" style="height: 100px;width: 100px;margin-left: 20px;margin-right: 20px">
-    <p class="welcome-text">宠物领养申请表</p>
+    <img src="./photos/狗狗头像.jpeg" style="height: 100px;width: 100px;margin-left: 20px;margin-right: 20px; border-radius: 50%;">
+    <p class="welcome-text">领养申请表</p>
   </div>
-  <el-form :model="form" label-width="200px">
-    <el-form-item label="领养人姓名">
-      <el-input v-model="form.name" />
+<el-form :model="form" label-width="200px">
+    <el-form-item label="领养人ID">
+      <el-input v-model="UID" readonly style="width: 600px;" />
+    </el-form-item>
+    <el-form-item label="领养人用户名">
+      <el-input v-model="U_name" readonly style="width: 600px;" />
+    </el-form-item>
+    <el-form-item label="宠物ID">
+      <el-input v-model="PID" readonly style="width: 600px;" />
+    </el-form-item>
+    <el-form-item label="宠物名字">
+      <el-input v-model="P_name" readonly style="width: 600px;" />
     </el-form-item>
     <el-form-item label="领养人性别">
       <el-radio-group v-model="form.gender">
-        <el-radio label="男" />
-        <el-radio label="女" />
+        <el-radio label="男" :value="true"/>
+        <el-radio label="女" :value="false"/>
       </el-radio-group>
     </el-form-item>
-    <el-form-item label="领养人手机号">
-      <el-input v-model="form.phone" />
-    </el-form-item>
-    <el-form-item label="领养时间">
+    <el-form-item label="有几年饲养经验">
       <el-col :span="11">
-        <el-date-picker
-          v-model="form.date"
-          type="date"
-          placeholder="选择日期"
-          style="width: 100%"
-        />
-      </el-col>
-    </el-form-item>
-    <el-form-item label="有无猫狗饲养经验">
-      <el-radio-group v-model="form.choice1">
-        <el-radio label="有" />
-        <el-radio label="无" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="有几年饲养经验" v-if="form.choice1 === '有'">
-      <el-col :span="11">
-        <el-input-number v-model="form.choice2" :min="0" :max="100" />
+        <el-input-number v-model="form.pet_exp" :min="0" :max="100" />
       </el-col>
     </el-form-item>
     <el-form-item label="是否可以坚持长期养护">
-      <el-radio-group v-model="form.choice3">
-        <el-radio label="是" />
-        <el-radio label="否" />
+      <el-radio-group v-model="form.long_term_care">
+        <el-radio label="是" :value="true"/>
+        <el-radio label="否" :value="false"/>
       </el-radio-group>
     </el-form-item>
     <el-form-item label="宠物生病是否愿意为它治病">
-      <el-radio-group v-model="form.choice4">
-        <el-radio label="是" />
-        <el-radio label="否" />
+      <el-radio-group v-model="form.w_to_treat">
+        <el-radio label="是" :value="true"/>
+        <el-radio label="否" :value="false"/>
       </el-radio-group>
     </el-form-item>
     <el-form-item label="每天能留出多少时间照顾猫狗">
       <el-col :span="11">
-        <el-input-number v-model="form.choice5" :min="0" :max="24" />
+        <el-input-number v-model="form.d_care_h" :min="0" :max="24" />
       </el-col>
     </el-form-item>
     <el-form-item label="领养后主要照顾猫狗的人是谁">
-      <el-input v-model="form.choice6" type="textarea" />
+      <el-input v-model="form.P_caregiver" style="width: 600px;"/>
     </el-form-item>
     <el-form-item label="家庭居住人口">
       <el-col :span="11">
-        <el-input-number v-model="form.choice7" :min="0" :max="100" />
+        <el-input-number v-model="form.f_popul" :min="0" :max="100" />
       </el-col>
     </el-form-item>
     <el-form-item label="家庭中是否有孩子">
-      <el-radio-group v-model="form.choice8">
-        <el-radio label="是" />
-        <el-radio label="否" />
+      <el-radio-group v-model="form.be_children">
+        <el-radio label="是" :value="true"/>
+        <el-radio label="否" :value="false"/>
       </el-radio-group>
     </el-form-item>
     <el-form-item label="是否愿意接受志愿者微信（视频、照片）的上门回访">
-      <el-radio-group v-model="form.choice9">
-        <el-radio label="是" />
-        <el-radio label="否" />
+      <el-radio-group v-model="form.accept_vis">
+        <el-radio label="是" :value="true"/>
+        <el-radio label="否" :value="false"/>
       </el-radio-group>
     </el-form-item>
     <el-form-item>
@@ -82,27 +72,66 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import axios from 'axios';
+import { useUserStore } from '@/store/user';
+import petadopt from '@/api/pet_adopt'; 
+
+const userStore = useUserStore(); // 请确保导入 useUserStore
 
 // do not use same name with ref
 const form = reactive({
-  name: '',
-  gender: '',
-  phone: '',
-  date: '',
-  choice1: '',
-  choice2: 0,
-  choice3: '',
-  choice4: '',
-  choice5: 0,
-  choice6: '',
-  choice7: 0,
-  choice8: '',
-  choice9: '',
+  gender: true,
+  pet_exp: 0,
+  long_term_care: true,
+  w_to_treat: true,
+  d_care_h: 0,
+  P_caregiver: '',
+  f_popul: 0,
+  be_children: true,
+  accept_vis: true,
 })
+
+const UID = userStore.userInfo.User_ID;
+const U_name = userStore.userInfo.User_Name;//这里参数名写对了吗？
+const PID = route.params.pet_simple.id;
+const P_name = route.params.pet_simple.name;
 
 const onSubmit = () => {
   console.log('submit!')
+  ElMessageBox.confirm(
+      '点击确认将提交申请，要继续吗？',
+      '确认',
+      {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning',
+      }
+    )
+    .then(() => {
+        petadopt.submitAdoptApplication(UID, PID, form)
+              .then(response => {
+                // 处理后端返回的响应
+                ElMessage({
+                  type: 'success',
+                  message: '提交成功',
+                });
+              })
+              .catch(error => {
+                // 处理错误
+                ElMessage({
+                  type: 'error',
+                  message: '提交失败',
+                });
+              });
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: '取消提交',
+        })
+      })
 }
 </script>
 
