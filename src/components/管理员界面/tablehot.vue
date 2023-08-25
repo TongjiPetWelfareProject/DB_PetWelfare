a1720912465b82c8a559adee3e39eb52ea99e1be
charlotteyanggg <104919289+llRagdoll@users.noreply.github.com>
2023-08-25 17:24:11 +0800

    管理员界面显示当前人气榜

--- src/components/管理员界面/tablehot.vue
+++ src/components/管理员界面/tablehot.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
     <el-table ref="tableRef" :data="tableData" style="width: 100%;border-radius:10px;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);" @selection-change="handleSelectionChange">
-      <el-table-column prop="id" label="宠物ID" width="200" />
-      <el-table-column prop="name" label="宠物名字" width="200" />
-      <el-table-column prop="views" label="阅读量" width="220" sortable :sort-method="sortViews" />
-      <el-table-column prop="likes" label="点赞量" width="220" sortable :sort-method="sortLikes" />
+      <el-table-column prop="id" label="宠物ID" width="200" align="center"/>
+      <el-table-column prop="name" label="宠物名字" width="200" align="center"/>
+      <el-table-column prop="views" label="阅读量" width="220" sortable :sort-method="sortViews" align="center"/>
+      <el-table-column prop="likes" label="点赞量" width="220" sortable :sort-method="sortLikes" align="center"/>
       <el-table-column type="selection" width="155" />
-      <!-- <el-table-column label="操作" width="180">
-        <template v-slot="scope">
-          <el-button size="mini" type="danger" @click="deleteRow(scope.row)">删除</el-button>
-        </template>
-      </el-table-column> -->
     </el-table>
     <br>
     <el-button type="primary" @click="publishPopular">发布</el-button>
+    <br>
+    <div class="currenthot">
+      <br>
+      <el-text class="welcome">当前人气榜:</el-text>
+      &nbsp;&nbsp;&nbsp;
+      <br>
+      <el-tag class="mx-1" size="large" type="warning">{{ topThreePetNames[0] }}</el-tag>
+      <el-tag class="mx-2" size="large" type="success">{{ topThreePetNames[1] }}</el-tag>
+      <el-tag class="mx-3" size="large">{{ topThreePetNames[2] }}</el-tag>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref,onMounted} from 'vue'
+import { ref,onMounted,computed} from 'vue'
 import { ElTable, ElMessageBox, ElButton } from 'element-plus'
 import gg_rqb_jk from '@/api/gg_rqb_jk'
 
@@ -31,59 +36,56 @@ export default {
   setup() {
     const tableRef = ref(null)
     const tableData = ref([
-      // { id: "001", name: '点点', views: 100, likes: 50 },
-      // { id: "002", name: '米多', views: 200, likes: 80 },
-      // { id: "003", name: '胡儿', views: 150, likes: 60 },
-      // { id: "004", name: 'ponki', views: 120, likes: 70 },
-      // { id: "005", name: 'rudy', views: 180, likes: 90 },
-      // { id: "006", name: '点点', views: 100, likes: 50 },
-      // { id: "007", name: '米多', views: 200, likes: 80 },
-      // { id: "008", name: '胡儿', views: 150, likes: 60 },
-      // { id: "009", name: 'ponki', views: 120, likes: 70 },
-      // { id: "010", name: 'rudy', views: 180, likes: 90 },
     ])
     const selectedPetIds = ref([]);//存储选择的三个人气宠物
-    const PopularPets = ref([]);//存储当前的人气榜宠物
+    const topThreePetNames = ref([]);//存储当前的人气榜宠物
 
     const handleSelectionChange = (selectedItems) => {
-    //  console.log(selectedItems)
-    }
+      selectedPetIds.value = selectedItems.map(pet => pet.id);
+    };
 
     const sortViews = (a, b) => {
-      return a.views - b.views
+      return b.views - a.views
     }
 
     const sortLikes = (a, b) => {
-      return a.likes - b.likes
+      return b.likes - a.likes
     }
 
-    const deleteRow = (row) => {
-      ElMessageBox.confirm('确定删除该宠物吗？', '提示', {
-        type: 'warning',
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-      }).then(() => {
-        // 删除操作
-        const index = tableData.value.indexOf(row)
-        if (index !== -1) {
-          tableData.value.splice(index, 1)
-        }
-      }).catch(() => {
-        // 取消删除
-      })
-    }
+    const updateTopThreePetNames = () => {
+
+      selectedPetIds.value.sort((a, b) => {
+        const petA = tableData.value.find(pet => pet.id === a);
+        const petB = tableData.value.find(pet => pet.id === b);
+        return (petB.views + petB.likes) - (petA.views + petA.likes);
+      });
+
+      
+      topThreePetNames.value = selectedPetIds.value.slice(0, 3).map(id => {
+        const pet = tableData.value.find(pet => pet.id === id);
+        return pet.name;
+      });
+    };
 
     function publishPopular() {
-      // 调用发送宠物ID数组到后端的API函数
-      gg_rqb_jk.publishPopularAPI(selectedPetIds.value)
-        .then(response => {
-          console.log('发布人气榜成功', response);
-          // 在这里可以根据后端返回的数据进行相应的操作
-        })
-        .catch(error => {
-          console.error('发布人气榜失败', error);
-        });
+
+    if (selectedPetIds.value.length !== 3) {
+      ElMessageBox.alert('请选择三只宠物作为人气榜宠物', '提示', {
+        confirmButtonText: '确定',
+      });
+      return;
     }
+
+    updateTopThreePetNames();
+    // 调用发送宠物ID数组到后端的API函数
+    gg_rqb_jk.publishPopularAPI(selectedPetIds.value)
+      .then(response => {
+        console.log('发布人气榜成功', response);
+      })
+      .catch(error => {
+        console.error('发布人气榜失败', error);
+      });
+}
 
     onMounted(async () => {
       try {
@@ -101,9 +103,46 @@ export default {
       handleSelectionChange,
       sortViews,
       sortLikes,
-      deleteRow,
       publishPopular,
+      topThreePetNames 
     }
   },
 }
 </script>
+
+
+<style scoped>
+
+.currenthot{
+  display:flex;
+  align-items: center;
+  margin-top: 15px;
+}
+.welcome{
+  margin-top: 10px;
+  float: left;
+  font-size: 15px;
+  font-weight: bold;
+  color: rgb(131, 131, 131);
+}
+
+
+.el-tag{
+  margin-left:20px;
+  /* justify-content: center;
+  align-items: center;
+  display: flex; */
+}
+
+/* .mx-1{
+  background-color: rgb(249, 184, 184);
+}
+.mx-2{
+  background-color: rgb(249, 220, 184);
+}
+.mx-3{
+  background-color: rgb(249, 248, 184);
+} */
+
+</style>
+
