abfaf93d326a278ae6b905c309650c35a8bbc13b
tuesaime <zsy18054799790@outlook.com>
2023-08-27 10:31:59 +0800

    更新管理员员工和医生

--- src/components/管理员界面/mytabledoctor.vue
+++ src/components/管理员界面/mytabledoctor.vue
@@ -1,29 +1,83 @@
 <template>
-  <el-table :data="tableData" style="width: 100%;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);border-radius: 10px;"
-    max-height="500">
-    <el-table-column prop="id" label="医生ID" width="120" />
-    <el-table-column prop="name" label="医生姓名" width="120" />
-    <el-table-column prop="phone" label="电话" width="120" />
-    <el-table-column prop="workingHours" label="工作时间" width="120" />
-    <el-table-column prop="salary" label="工资" width="170" />
-    <el-table-column label="操作" width="360">
-      <template #default="scope">
-        <el-button link type="primary" size="small" @click.prevent="editRow(scope.$index)">
-          编辑
-        </el-button>
-        <el-button link type="danger" size="small" @click.prevent="deleteRow(scope.$index)">
-          删除
-        </el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <br />
-  <el-button type="primary" @click="addRow">添加医生</el-button>
+  <div>
+    <el-table :data="tableData" style="width: 100%;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);border-radius: 10px;"
+      max-height="500">
+      <el-table-column prop="id" label="医生ID" width="120" />
+      <el-table-column prop="name" label="医生姓名" width="120" />
+      <el-table-column prop="phone" label="电话" width="120" />
+      <el-table-column prop="workingHours" label="工作时间" width="120" />
+      <el-table-column prop="salary" label="工资" width="170" />
+      <el-table-column label="操作" width="160">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click.prevent="editRow(scope.$index)">
+            编辑
+          </el-button>
+          <el-button link type="danger" size="small" @click.prevent="deleteRow(scope.$index)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <br />
+    <el-button type="primary" @click="addRow">添加医生</el-button>
+
+    <!-- Edit Doctor Dialog -->
+    <el-dialog v-model="editDialogVisible" title="编辑医生信息" @close="resetEditDialog">
+      <el-form :model="editedDoctor" label-width="80px">
+        <!-- 表单内容 -->
+        <el-form-item label="医生ID">
+          <el-input v-model="editedDoctor.id"></el-input>
+        </el-form-item>
+        <el-form-item label="医生姓名">
+          <el-input v-model="editedDoctor.name"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="editedDoctor.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="工作时间">
+          <el-input v-model="editedDoctor.workingHours"></el-input>
+        </el-form-item>
+        <el-form-item label="工资">
+          <el-input v-model="editedDoctor.salary"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitEditedDoctor">保存</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- Add Doctor Dialog -->
+    <el-dialog v-model="addDialogVisible" title="添加医生" @close="resetAddDialog">
+      <el-form :model="newDoctor" label-width="80px">
+        <!-- 表单内容 -->
+        <el-form-item label="医生ID">
+          <el-input v-model="newDoctor.id"></el-input>
+        </el-form-item>
+        <el-form-item label="医生姓名">
+          <el-input v-model="newDoctor.name"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="newDoctor.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="工作时间">
+          <el-input v-model="newDoctor.workingHours"></el-input>
+        </el-form-item>
+        <el-form-item label="工资">
+          <el-input v-model="newDoctor.salary"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitNewDoctor">添加</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
-    
+
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import { ElButton, ElTable, ElTableColumn } from 'element-plus';
+import { ElButton, ElTable, ElDialog, ElForm, ElInput } from 'element-plus';
 import axios from 'axios';
 
 interface Doctor {
@@ -35,6 +89,26 @@ interface Doctor {
 }
 
 const tableData = ref<Doctor[]>([]);
+const editDialogVisible = ref(false);
+const addDialogVisible = ref(false);
+const editedDoctor = ref<Doctor>({
+  id: '',
+  name: '',
+  phone: '',
+  workingHours: '',
+  salary: '',
+});
+const newDoctor = ref<Doctor>({
+  id: '',
+  name: '',
+  phone: '',
+  workingHours: '',
+  salary: '',
+});
+
+onMounted(async () => {
+  await fetchData();
+});
 
 const fetchData = async () => {
   try {
@@ -45,13 +119,11 @@ const fetchData = async () => {
   }
 };
 
-onMounted(fetchData);
-
 const deleteRow = (index: number) => {
   const doctorId = tableData.value[index].id;
-  axios.delete(`/api/doctor/${doctorId}`) // 使用DELETE请求删除指定医生数据
+  axios.delete(`/api/delete-doctor/${doctorId}`)
     .then(() => {
-      tableData.value.splice(index, 1); // 从tableData中移除已删除的医生数据
+      tableData.value.splice(index, 1);
     })
     .catch(error => {
       console.error('删除数据时出错：', error);
@@ -59,35 +131,56 @@ const deleteRow = (index: number) => {
 };
 
 const editRow = (index: number) => {
-  const doctor = tableData.value[index];
-  const doctorId = doctor.id;
-  axios.put(`/api/doctor/${doctorId}`, doctor) // 使用PUT请求更新指定医生数据
+  editedDoctor.value = { ...tableData.value[index] };
+  editDialogVisible.value = true;
+};
+
+const submitEditedDoctor = () => {
+  axios.put(`/api/edit-doctor/${editedDoctor.value.id}`, editedDoctor.value)
     .then(() => {
-      // 更新成功的处理逻辑
+      // 编辑成功的处理逻辑
+      editDialogVisible.value = false;
     })
     .catch(error => {
       console.error('编辑数据时出错:', error);
     });
 };
 
-const addRow = () => {
-  const newDoctor = {
+const resetEditDialog = () => {
+  editedDoctor.value = {
     id: '',
     name: '',
     phone: '',
     workingHours: '',
     salary: '',
   };
+};
 
-  axios.post('/api/doctor', newDoctor) // 使用POST请求添加医生数据
+const addRow = () => {
+  addDialogVisible.value = true;
+};
+
+const submitNewDoctor = () => {
+  axios.post('/api/add-doctor', newDoctor.value)
     .then(response => {
       const newDoctorId = response.data.id;
-      newDoctor.id = newDoctorId; // 将新的医生ID赋值给newDoctor对象
-      tableData.value.push(newDoctor); // 将新的医生数据添加到tableData中
+      newDoctor.value.id = newDoctorId;
+      tableData.value.push(newDoctor.value);
+      addDialogVisible.value = false;
     })
     .catch(error => {
       console.error('添加数据时出错：', error);
     });
+};
+
+const resetAddDialog = () => {
+  newDoctor.value = {
+    id: '',
+    name: '',
+    phone: '',
+    workingHours: '',
+    salary: '',
+  };
 };
 
 </script>
--- src/components/管理员界面/mytableemployee.vue
+++ src/components/管理员界面/mytableemployee.vue
@@ -1,30 +1,88 @@
 <template>
-    <el-table :data="tableData" style="width: 100%;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);border-radius: 10px;"
-        max-height="600">
-        <el-table-column prop="id" label="员工ID" width="120" />
-        <el-table-column prop="name" label="员工姓名" width="120" />
-        <el-table-column prop="phone" label="电话" width="120" />
-        <el-table-column prop="responsibility" label="职责" width="120" />
-        <el-table-column prop="workingHours" label="工作时间" width="120" />
-        <el-table-column prop="salary" label="工资" width="170" />
-        <el-table-column label="操作" width="360">
-            <template #default="scope">
-                <el-button link type="primary" size="small" @click.prevent="editRow(scope.$index)">
-                    编辑
-                </el-button>
-                <el-button link type="danger" size="small" @click.prevent="deleteRow(scope.$index)">
-                    删除
-                </el-button>
-            </template>
-        </el-table-column>
-    </el-table>
-    <br />
-    <el-button type="primary" @click="addRow">添加员工</el-button>
+    <div>
+        <el-table :data="tableData" style="width: 100%;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);border-radius: 10px;"
+            max-height="600">
+            <el-table-column prop="id" label="员工ID" width="120" />
+            <el-table-column prop="name" label="员工姓名" width="120" />
+            <el-table-column prop="phone" label="电话" width="120" />
+            <el-table-column prop="responsibility" label="职责" width="120" />
+            <el-table-column prop="workingHours" label="工作时间" width="120" />
+            <el-table-column prop="salary" label="工资" width="170" />
+            <el-table-column label="操作" width="360">
+                <template #default="scope">
+                    <el-button link type="primary" size="small" @click.prevent="editRow(scope.$index)">
+                        编辑
+                    </el-button>
+                    <el-button link type="danger" size="small" @click.prevent="deleteRow(scope.$index)">
+                        删除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <br />
+        <el-button type="primary" @click="addRow">添加员工</el-button>
+
+        <!-- Edit Employee Dialog -->
+        <el-dialog v-model="editDialogVisible" title="编辑员工信息" @close="resetEditDialog">
+            <el-form :model="editedEmployee" label-width="80px">
+                <el-form-item label="员工ID">
+                    <el-input v-model="editedEmployee.id"></el-input>
+                </el-form-item>
+                <el-form-item label="员工姓名">
+                    <el-input v-model="editedEmployee.name"></el-input>
+                </el-form-item>
+                <el-form-item label="电话">
+                    <el-input v-model="editedEmployee.phone"></el-input>
+                </el-form-item>
+                <el-form-item label="职责">
+                    <el-input v-model="editedEmployee.responsibility"></el-input>
+                </el-form-item>
+                <el-form-item label="工作时间">
+                    <el-input v-model="editedEmployee.workingHours"></el-input>
+                </el-form-item>
+                <el-form-item label="工资">
+                    <el-input v-model="editedEmployee.salary"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="editDialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="submitEditedEmployee">保存</el-button>
+            </div>
+        </el-dialog>
+
+        <!-- Add Employee Dialog -->
+        <el-dialog v-model="addDialogVisible" title="添加员工" @close="resetAddDialog">
+            <el-form :model="newEmployee" label-width="80px">
+                <el-form-item label="员工ID">
+                    <el-input v-model="newEmployee.id"></el-input>
+                </el-form-item>
+                <el-form-item label="员工姓名">
+                    <el-input v-model="newEmployee.name"></el-input>
+                </el-form-item>
+                <el-form-item label="电话">
+                    <el-input v-model="newEmployee.phone"></el-input>
+                </el-form-item>
+                <el-form-item label="职责">
+                    <el-input v-model="newEmployee.responsibility"></el-input>
+                </el-form-item>
+                <el-form-item label="工作时间">
+                    <el-input v-model="newEmployee.workingHours"></el-input>
+                </el-form-item>
+                <el-form-item label="工资">
+                    <el-input v-model="newEmployee.salary"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="addDialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="submitNewEmployee">添加</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
-    
+
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import { ElButton, ElTable, ElTableColumn } from 'element-plus';
+import { ElButton, ElTable, ElDialog, ElForm, ElInput } from 'element-plus';
 import axios from 'axios';
 
 interface Employee {
@@ -37,6 +95,28 @@ interface Employee {
 }
 
 const tableData = ref<Employee[]>([]);
+const editDialogVisible = ref(false);
+const addDialogVisible = ref(false);
+const editedEmployee = ref<Employee>({
+    id: '',
+    name: '',
+    phone: '',
+    responsibility: '',
+    workingHours: '',
+    salary: '',
+});
+const newEmployee = ref<Employee>({
+    id: '',
+    name: '',
+    phone: '',
+    responsibility: '',
+    workingHours: '',
+    salary: '',
+});
+
+onMounted(async () => {
+    await fetchData();
+});
 
 const fetchData = async () => {
     try {
@@ -47,11 +127,9 @@ const fetchData = async () => {
     }
 };
 
-onMounted(fetchData);
-
 const deleteRow = (index: number) => {
     const employeeId = tableData.value[index].id;
-    axios.delete(`/api/employee/${employeeId}`)
+    axios.delete(`/api/delete-employee/${employeeId}`)
         .then(() => {
             tableData.value.splice(index, 1);
         })
@@ -61,19 +139,23 @@ const deleteRow = (index: number) => {
 };
 
 const editRow = (index: number) => {
-    const employee = tableData.value[index];
-    const employeeId = employee.id;
-    axios.put(`/api/employee/${employeeId}`, employee)
+    editedEmployee.value = { ...tableData.value[index] };
+    editDialogVisible.value = true;
+};
+
+const submitEditedEmployee = () => {
+    axios.put(`/api/edit-employee/${editedEmployee.value.id}`, editedEmployee.value)
         .then(() => {
-            // Handle successful update
+            // 编辑成功的处理逻辑
+            editDialogVisible.value = false;
         })
         .catch(error => {
-            console.error('编辑数据时出错：', error);
+            console.error('编辑数据时出错:', error);
         });
 };
 
-const addRow = () => {
-    const newEmployee: Employee = {
+const resetEditDialog = () => {
+    editedEmployee.value = {
         id: '',
         name: '',
         phone: '',
@@ -81,16 +163,34 @@ const addRow = () => {
         workingHours: '',
         salary: '',
     };
+};
 
-    axios.post('/api/addemployee', newEmployee)
+const addRow = () => {
+    addDialogVisible.value = true;
+};
+
+const submitNewEmployee = () => {
+    axios.post('/api/add-employee', newEmployee.value)
         .then(response => {
             const newEmployeeId = response.data.id;
-            newEmployee.id = newEmployeeId;
-            tableData.value.push(newEmployee);
+            newEmployee.value.id = newEmployeeId;
+            tableData.value.push(newEmployee.value);
+            addDialogVisible.value = false;
         })
         .catch(error => {
             console.error('添加数据时出错：', error);
         });
+};
+
+const resetAddDialog = () => {
+    newEmployee.value = {
+        id: '',
+        name: '',
+        phone: '',
+        responsibility: '',
+        workingHours: '',
+        salary: '',
+    };
 };
 
 </script>
