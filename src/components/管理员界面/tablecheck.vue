19b49edc7d315b47192693b547bb5467f2b7c061
Victor Hu <2151409@tongji.edu.cn>
2023-09-02 18:29:22 +0800

    Revert "完善审核部分的帖子内容显示"
    This reverts commit a3bb8d687fef1701eda40d4e85ca126a4840658f.

--- src/components/管理员界面/tablecheck.vue
+++ src/components/管理员界面/tablecheck.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <el-table ref="tableRef" :data="tableData" style="width: 100%;border-radius:10px;box-shadow: 0 0px 4px rgba(66, 66, 66, 0.2);">
-      <el-table-column prop="postId" label="帖子ID" width="80"></el-table-column>
-      <el-table-column prop="employeeId" label="用户ID" width="120"></el-table-column>
+    <el-table ref="tableRef" :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table-column prop="postId" label="帖子ID" width="180"></el-table-column>
+      <el-table-column prop="employeeId" label="用户ID" width="180"></el-table-column>
       <el-table-column prop="postTime" label="发帖时间" width="200" sortable :sort-method="sortTime"></el-table-column>
-      <el-table-column label="内容" width="500" align="center">
-        <template v-slot="{ row }">
-          <div class="announcement-cell">
-            <el-button type="text" @click="showContent(row)">
-              {{ getShortenedContent(row.content) }}
-            </el-button>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="是否通过" width="120">
+      <el-table-column prop="content" label="内容" width="220"></el-table-column>
+      <el-table-column label="是否通过" width="180">
         <template v-slot="scope">
           <el-button size="mini" type="primary" @click="approveApplication(scope.$index)">Y</el-button>
           <el-button size="mini" type="danger" @click="rejectApplication(scope.$index)">N</el-button>
@@ -38,28 +30,13 @@ export default {
   setup() {
     const tableRef = ref(null)
     const tableData = ref([])
+    const handleSelectionChange = (selectedItems) => {
+      console.log(selectedItems)
+    }
 
     const sortTime = (a, b) => {
       return new Date(a.postTime) - new Date(b.postTime)
     }
-
-    function showContent(row) {
-      ElMessageBox.alert(row.content, '内容', {
-        confirmButtonText: '关闭',
-      });
-    }
-
-    function getShortenedContent(content) {
-      const lines = content.split('\n');
-      const maxLines = 1;
-      
-      if (lines.length > maxLines) {
-        return lines.slice(0, maxLines).join('\n');
-      }
-      
-      return content;
-    }
-
 
     onMounted(async () => {
       try {
@@ -68,7 +45,7 @@ export default {
       } catch (error) {
         console.error('获取数据时出错：', error);
       }
-    })
+    });
 
     const approveApplication = async(index) => {
       const infoToUpdate = tableData.value[index];
@@ -104,7 +81,7 @@ export default {
       } catch (error) {
         console.error('获取数据时出错：', error);
       }
-    }
+    };
 
     return {
       tableRef,
