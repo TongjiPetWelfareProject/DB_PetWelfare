85afea717d81608fbc295df8e16ee407a4713426
LuminousWanderer <1664524275@qq.com>
2023-09-05 12:15:49 +0800

    登录错误信息详情展示

--- src/util/http.js
+++ src/util/http.js
@@ -17,10 +17,31 @@ httpInstance.interceptors.request.use(config => {
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data,e =>{
     // 统一错误提示
-    ElMessage({
-        type:'warning',
-        message:'账号、密码输入错误，或已被封号，请重新尝试或联系管理员'
-    })
+    console.log(e.response.data)
+    if(e.response.data === -1){
+        ElMessage({
+            type:'warning',
+            message:'密码错误，请重新输入'
+        })
+    }else if (e.response.data === -2) {
+        // 处理 e.response.data === -2 的情况
+        ElMessage({
+          type: 'warning',
+          message: '账号已被封禁，请等待解禁',
+        });
+      } else if (e.response.data === -3) {
+        // 处理 e.response.data === -3 的情况
+        ElMessage({
+          type: 'warning',
+          message: '用户不存在',
+        });
+      } else {
+        ElMessage({
+            type: 'warning',
+            message: '服务器出错',
+          });
+      }
+    
     return Promise.reject(e)
 })
 
